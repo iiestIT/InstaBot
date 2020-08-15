5bd14c557f6bde6b40ccf1729f91324f3e1bb5d3
iiestIT <it.iiest.de@gmail.com>
2020-08-15 19:28:29 +0200

    add login (https://github.com/iiestIT/InstaBot/projects/1#card-43714380)

--- insta_bot/bot/bot.py
+++ insta_bot/bot/bot.py
@@ -1,19 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-
-from typing import List, Dict, Optional, Union, Any
-from pydantic import BaseModel
+import time
 
 
-class Bot(BaseModel):
-    def __int__(self, driver: Any, username: str):
-        self.driver = driver
+class Bot:
+    start_url = "https://www.instagram.com/"
+
+    def __init__(self, path: str, username: str):  # TODO: add more browser
+        """
+        :param path: absolut path to a webdriver
+        :param username: a valid instagram username
+        """
+
         self.username = username
-        self.start_url: str = "https://www.instagram.com/"
+        self.driver = webdriver.Chrome(path)
 
     def login(self, password: str):
-        pass
+        self.driver.get(self.start_url)
+        time.sleep(5)
+        user_field = self.driver.find_element_by_xpath('//input[@name="username"]')
+        user_field.send_keys(self.username)
+        passwd_field = self.driver.find_element_by_xpath('//input[@name="password"]')
+        passwd_field.send_keys(password)
+        submit_field = self.driver.find_element_by_xpath('//button[@type="submit"]')
+        time.sleep(1)
+        submit_field.send_keys(Keys.ENTER)
+        time.sleep(5)
+        if self.driver.find_element_by_xpath('//section[@class="ABCxa"]'):
+            nn = self.driver.find_element_by_xpath('//div[@class="cmbtv"]/button')
+            nn.send_keys(Keys.ENTER)
+            time.sleep(3)
+        if self.driver.find_element_by_xpath('//div[@class="_1XyCr"]'):
+            nn = self.driver.find_element_by_xpath('//div[@class="mt3GC"]/button[2]')
+            nn.send_keys(Keys.ENTER)
+            time.sleep(1)
 
     def logout(self):
         pass
