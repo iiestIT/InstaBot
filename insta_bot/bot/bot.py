d6b25c9084a9d63422816ca6d04d1f336bf399d1
iiestIT <it.iiest.de@gmail.com>
2020-08-16 13:15:52 +0200

    add pm (https://github.com/iiestIT/InstaBot/projects/1#card-43714384)

--- insta_bot/bot/bot.py
+++ insta_bot/bot/bot.py
@@ -7,7 +7,7 @@ import time
 class Bot:
     start_url = "https://www.instagram.com/"
 
-    def __init__(self, path: str, username: str):  # TODO: add more browser
+    def __init__(self, path: str, username: str):  # TODO: add more browser support
         """
         :param path: absolut path to a webdriver
         :param username: a valid instagram username
@@ -16,38 +16,73 @@ class Bot:
         self.username = username
         self.driver = webdriver.Chrome(path)
 
-    def login(self, password: str):
+    def login(self, password: str) -> bool:
+        """
+        :param password: put in a valid instagram password in plaintext
+        :return: bool
+        """
         self.driver.get(self.start_url)
         time.sleep(5)
-        user_field = self.driver.find_element_by_xpath('//input[@name="username"]')
-        user_field.send_keys(self.username)
-        passwd_field = self.driver.find_element_by_xpath('//input[@name="password"]')
-        passwd_field.send_keys(password)
-        submit_field = self.driver.find_element_by_xpath('//button[@type="submit"]')
-        time.sleep(1)
-        submit_field.send_keys(Keys.ENTER)
-        time.sleep(5)
-        if self.driver.find_element_by_xpath('//section[@class="ABCxa"]'):
-            nn = self.driver.find_element_by_xpath('//div[@class="cmbtv"]/button')
-            nn.send_keys(Keys.ENTER)
-            time.sleep(3)
-        if self.driver.find_element_by_xpath('//div[@class="_1XyCr"]'):
-            nn = self.driver.find_element_by_xpath('//div[@class="mt3GC"]/button[2]')
-            nn.send_keys(Keys.ENTER)
+        try:
+            user_field = self.driver.find_element_by_xpath('//input[@name="username"]')
+            user_field.send_keys(self.username)
+            passwd_field = self.driver.find_element_by_xpath('//input[@name="password"]')
+            passwd_field.send_keys(password)
+            submit_field = self.driver.find_element_by_xpath('//button[@type="submit"]')
             time.sleep(1)
+            submit_field.send_keys(Keys.ENTER)
+            time.sleep(5)
+            if self.driver.find_element_by_xpath('//section[@class="ABCxa"]'):
+                nn = self.driver.find_element_by_xpath('//div[@class="cmbtv"]/button')
+                nn.send_keys(Keys.ENTER)
+                time.sleep(3)
+            if self.driver.find_element_by_xpath('//div[@class="_1XyCr"]'):
+                nn = self.driver.find_element_by_xpath('//div[@class="mt3GC"]/button[2]')
+                nn.send_keys(Keys.ENTER)
+                time.sleep(1)
+            return True
+        except:
+            return False
 
-    def logout(self):
+    def logout(self) -> bool:
+        """
+        :return: bool
+        """
         menu = self.driver.find_element_by_xpath('//span[@class="_2dbep qNELH"]')
         menu.click()
         time.sleep(1)
         logout = self.driver.find_element_by_xpath('//div[@class="-qQT3"]')
         logout.click()
+        return True
 
-    def pm(self):
+    def pm(self, msg: str, user_url=None, username=None) -> bool:
+        """
+        :param msg: the message itself
+        :param user_url: profile url of the target
+        :param username: targets username (experimental) !!!!
+        :return: bool
+        """
+        msg = str(msg)
+        if username:
+            url = f"{self.start_url}{username}/"
+        if user_url:
+            url = user_url
+        try:
+            self.driver.get(url)
+            time.sleep(4)
+            sm = self.driver.find_element_by_xpath('//div[@class="_862NM"]//button')
+            sm.click()
+            time.sleep(4)
+            tf = self.driver.find_element_by_xpath('//div[@class="X3a-9"]//textarea')
+            tf.send_keys(msg)
+            time.sleep(1)
+            tf.send_keys(Keys.ENTER)
+            return True
+        except:
+            return False
+
+    def like(self) -> bool:
         pass
 
-    def like(self):
-        pass
-
-    def commend(self):
+    def commend(self) -> bool:
         pass
